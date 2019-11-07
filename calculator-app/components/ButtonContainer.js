e3da28b95891f4b1aba951d232dc5d0ce96ddc72
MicahJank <MicahJank@users.noreply.github.com>
2019-11-06 18:00:12 -0600

    button layout is mostly complete

--- calculator-app/components/ButtonContainer.js
+++ calculator-app/components/ButtonContainer.js
@@ -8,25 +8,35 @@ const ButtonContainer = () => {
 
     return (
         <View style={styles.container}>
-            <Button button={styles.otherButton} style={styles.otherText} buttonText={'AC'} />
-            <Button button={styles.otherButton} style={styles.otherText} buttonText={'+/-'} />
-            <Button button={styles.otherButton} style={styles.otherText} buttonText={'%'} />
-            <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'/'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'7'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'8'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'9'} />
-            <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'x'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'4'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'5'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'6'} />
-            <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'-'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'1'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'2'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'3'} />
-            <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'+'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'0'} />
-            <Button button={styles.numberButton} style={styles.numberText} buttonText={'.'} />
-            <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'='} />
+            <View style={styles.row}>
+                <Button button={styles.otherButton} style={styles.otherText} buttonText={'AC'} />
+                <Button button={styles.otherButton} style={styles.otherText} buttonText={'+/-'} />
+                <Button button={styles.otherButton} style={styles.otherText} buttonText={'%'} />
+                <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'/'} />
+            </View>
+            <View style={styles.row}>
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'7'} />
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'8'} />
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'9'} />
+                <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'x'} />
+            </View>
+            <View style={styles.row}>
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'4'} />
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'5'} />
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'6'} />
+                <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'-'} />
+            </View>
+            <View style={styles.row}>
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'1'} />
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'2'} />
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'3'} />
+                <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'+'} />
+            </View>
+            <View style={styles.row}>
+                <Button button={styles.zeroButton} style={styles.numberText} buttonText={'0'} />
+                <Button button={styles.numberButton} style={styles.numberText} buttonText={'.'} />
+                <Button button={styles.operatorButton} style={styles.operatorText} buttonText={'='} />
+            </View>
         </View>
     );
 };
@@ -34,10 +44,16 @@ const ButtonContainer = () => {
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: 'column',
         flexWrap: 'wrap',
         flex: 1,
-        justifyContent: 'space-between',
+        padding: 10
+    },
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        flex: 1
     },
     numberText: {
         color: '#fff',
@@ -58,30 +74,39 @@ const styles = StyleSheet.create({
         backgroundColor: '#333333',
         borderColor: '#fff',
         borderRadius: 50,
-        width: 75,
-        height: 75
+        flex: 1,
+        margin: 5
     },
     operatorButton: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#FE9F0A',
+        backgroundColor: '#FF9500',
         borderColor: '#fff',
         borderRadius: 50,
-        width: 75,
-        height: 75,
-        marginHorizontal: 5
+        flex: 1,
+        margin: 5
     },
     otherButton: {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#333333',
+        backgroundColor: '#D4D4D2',
         borderColor: '#000',
         borderRadius: 50,
-        width: 75,
-        height: 75,
-        marginHorizontal: 5
+        flex: 1,
+        margin: 5
+    },
+    zeroButton: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        backgroundColor: '#333333',
+        borderColor: '#fff',
+        borderRadius: 50,
+        flex: 2,
+        margin: 5,
+        paddingLeft: 30
     }
 })
 
