ac949fdb05e845978b37704f0d786f22d524e049
MicahJank <MicahJank@users.noreply.github.com>
2019-11-14 00:12:02 -0600

    still trying to figure out extending the decimal range for the percentage function

--- calculator-app/utils/operations.js
+++ calculator-app/utils/operations.js
@@ -1,4 +1,14 @@
+import { Decimal } from 'decimal.js';
+import * as math from 'mathjs';
 // a place to store all the calculator operation functions
+
+// const config = { };
+// const math = create(all, config);
+
+// math.config({
+//     number: 'BigNumber',
+//     precision: 64
+// });
 
 
 
@@ -6,7 +16,15 @@
 export const roundPercentage = num => { //TODO FIGURE OUT HOW TO GET ERROR TO RETURN WHEN NUMBER GOES PAST RANGE
     let newNum = num
     try {
-        newNum = Number.parseFloat(Number.parseFloat(num / 100).toFixed(20));
+        if(newNum < 1e-10) {
+            newNum = Number.parseFloat(newNum);
+            console.log(newNum);
+            newNum = math.evaluate(newNum / 100);
+            console.log(newNum);
+        } else {
+            newNum = Number.parseFloat(Number.parseFloat(num / 100).toFixed(20));
+        }
+        
     } catch (e) {
         if(e instanceof RangeError) {
             console.log(e, true);
