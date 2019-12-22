d4be321449e92c34ac1d7660fe642dd9efa0c3d4
MicahJank <MicahJank@users.noreply.github.com>
2019-12-21 21:40:39 -0600

    made an important change to the operations.js

--- calculator-app/utils/operations.js
+++ calculator-app/utils/operations.js
@@ -9,7 +9,7 @@ export const addNumbers = (num1, num2) => {
 
 // percentage rounding function
 export const roundPercentage = num => { //TODO FIGURE OUT HOW TO GET ERROR TO RETURN WHEN NUMBER GOES PAST RANGE
-    let newNum = num
+    let newNum = num;
     try {
         if(newNum < 1e-10) {
             newNum = Number.parseFloat(newNum);
